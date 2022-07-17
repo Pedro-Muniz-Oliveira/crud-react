0d331afc374a286e7d0493212c43c48b1b5e9d97
Pedro Muniz <67704930+Pedro-Muniz-Oliveira@users.noreply.github.com>
2022-07-17 16:45:22 -0300

    Primeiro teste ok Server/index.js
    https://www.youtube.com/watch?v=5_9rvyT9cg4  05:24 min

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+
 //Conexão ao mysql
 const mysql = require("mysql");
 
+const dp = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Pedro@2003",
+  database: "crud-na-pratica",
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello Word!!");
+  //res.send("Hello Word!!");
+
+  let SQL = "INSERT INTO users( nome, custo) values('Pedro', 35000)";
+
+  dp.query(SQL, (err, result) => {
+    console.log(err);
+    console.log(result);
+  });
 });
 
 app.listen(3011, () => {
   console.log("rodando servidor");
 });
+
+

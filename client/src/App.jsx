56b82feecac24562723581750a80b36d5905f82a
Pedro Muniz <67704930+Pedro-Muniz-Oliveira@users.noreply.github.com>
2022-07-17 18:18:35 -0300

    Primeiro insert into
    Rodou o insert

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
 	const [values, setValues] = useState();
@@ -7,23 +8,28 @@ function App() {
 	console.log(values)
 	const escreveValor = (value) => {
 		setValues((prevValue) => ({
-		    ...prevValue,
+			...prevValue,
 			[value.target.name]: value.target.value,
 		}));
 	};
 
-	const handleClickButton = () =>{
-		console.log(values);
-	}
+	const handleClickButton = () => {
+		Axios.post("http://localhost:3007/register", {
+			name: values.name,
+			cost: values.cost,
+		}).then((response) => {
+			console.log(response);
+		});
+	};
 
-return <div className=".app-container">
-	<div className="register-container">
-		<h1 className="register-title">Teste de CRUD</h1>
-		<input type="text" name="name" placeholder="Nome" className="register-input" onChange={escreveValor}></input>
-		<input type="text" name="cost" placeholder="Preço" className="register-input" onChange={escreveValor}></input>
-		<button className="register-button" onClick={() => handleClickButton()}>Cadastrar</button>
-	</div>
+	return <div className=".app-container">
+		<div className="register-container">
+			<h1 className="register-title">Teste de CRUD</h1>
+			<input type="text" name="name" placeholder="Nome" className="register-input" onChange={escreveValor}></input>
+			<input type="text" name="cost" placeholder="Preço" className="register-input" onChange={escreveValor}></input>
+			<button className="register-button" onClick={() => handleClickButton()}>Cadastrar</button>
+		</div>
 
-</div>;
+	</div>;
 }
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,29 +1,43 @@
 const express = require("express");
 const app = express();
-
 //Conexão ao mysql
 const mysql = require("mysql");
+const cors = require("cors");
+const { json } = require("express");
 
-const dp = mysql.createPool({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "Pedro@2003",
   database: "crud-na-pratica",
 });
 
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+
+  let SQL = "INSERT INTO users(nome,custo) VALUES(?,?)";
+   
+    db.query(SQL, [name,cost], (err, result) => {
+        console.log(err);
+    });
+
+  //console.log(name);
+});
+
+/* primeiro teste do banco
 app.get("/", (req, res) => {
   //res.send("Hello Word!!");
-
   let SQL = "INSERT INTO users( nome, custo) values('Pedro', 35000)";
-
   dp.query(SQL, (err, result) => {
     console.log(err);
-    console.log(result);
+    //console.log(result);
   });
-});
+});*/
 
-app.listen(3011, () => {
+app.listen(3007, () => {
   console.log("rodando servidor");
 });
-
-
